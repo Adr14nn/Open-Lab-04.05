5879a52b2d692c16e709dc2bb51ab6f2946841df
Adr144n <adiko1805@gmail.com>
2021-11-24 16:54:10 +0100

    Open-Lab-04.05

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string nasobenie = string.Empty;
+            char[] charArr = orig.ToCharArray();
+            foreach (char ch in charArr)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    nasobenie += ch;
+                }
+            }
+            return new string(nasobenie);
         }
     }
 }
